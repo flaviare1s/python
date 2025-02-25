58422235d461f99a17adc3cdb94fcafb6a174339
flaviare1s <flaviarster@gmail.com>
2025-02-25 16:27:17 -0300

    Exercícios 001 e 002

---
+++ lista-de-exercicios-01/ex001.py
@@ -0,0 +1,5 @@
+# Crie um programa que escreva "Olá, Mundo!" na tela.
+
+msg = 'Olá, Mundo!'
+
+print(msg)
---
+++ lista-de-exercicios-01/ex002.py
@@ -0,0 +1,3 @@
+nome = input('Digite seu nome: ')
+
+print(f'Olá {nome}, seja bem vindo(a) ao curso de Python!')
